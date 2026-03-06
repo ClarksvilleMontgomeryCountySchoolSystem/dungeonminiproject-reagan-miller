367ef623c26280c6ab513e064170c8f08795a092
reagan-miller <reagan.miller586@students.cmcss.net>
2026-03-05 18:51:32 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -4,3 +4,31 @@ if torch_lit:
 else:
     outcome = "Doom: death and despair await me."
 print(outcome)
+
+has_key = True
+if has_key:
+    outcome = "Click: there goes the lock."
+else:
+    outcome = "Doom: I can't get through."
+print(outcome)
+
+guard_awake = False
+if not guard_awake:
+    outcome = "Shadow: the guard will never catch me."
+else:
+    outcome = "Doom: there's no way to escape."
+print(outcome)
+
+drawbridge_raised = False
+if not drawbridge_raised:
+    outcome = "Thunder: not even the greatest storm could stop me."
+else:
+    outcome = "Doom: I have no way inside."
+print(outcome)
+
+escaped = True
+if escaped:
+    outcome = "Legend: my name will be remebered for centuries!"
+else:
+    outcome = "Doom: I beleive that I will die here."
+print(outcome)
